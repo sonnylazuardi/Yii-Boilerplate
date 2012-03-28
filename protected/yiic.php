7ead968f4cd3fa699fd2f332f8666d95ea597497
Florian Fackler <florian.fackler@mayflower.de>
2012-03-28 09:01:44 +0200

    Fixed console configuration

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,14 @@
 <?php
 
-// change the following paths if necessary
-$yiic=__DIR__.'/../protected/library/yii/yiic.php';
-$config=__DIR__.'/config/console.php';
+defined('SERVER_ENV') || define('SERVER_ENV', 'development');
+
+$yiic   = __DIR__ . '/library/yii/yiic.php';
+$config = __DIR__ . '/config/' . SERVER_ENV . '.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yiic);
